0e94a3b8edd4978e6b85056325e139f86fe2a57c
Jason Chen <tojasonchen@gmail.com>
2017-07-05 20:49:52 -0400

    add setup scripts

---
+++ setup/README.md
@@ -0,0 +1,4 @@
+Deep Learning environment/libraries Setup Scripts for Ubuntu 16.04 with GPU support (e.g. AWS p2.xlarge)
+
+Scripts are copied and modified from fast.ai online course:
+https://github.com/fastai/courses
---
+++ setup/install-other.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#pip install keras==1.2.2
+
+pip install tensorflow-gpu
+pip install kaggle-cli
+pip install PyStemmer
+
+#pip install spacy
+conda install -y -c conda-forge spacy
+python -m spacy.en.download
+
+sudo apt install htop
+sudo apt install p7zip-full
+sudo apt install -y zip
+sudo apt install unzip
